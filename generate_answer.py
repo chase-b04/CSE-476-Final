60590af30eecb7801be01f28700266756b0f97fc
cbulkin <chasebulkin@yahoo.com>
2025-12-06 21:35:22 -0700

    FINALLY GOT A FULL RUN WITH A JSON. Going to make some edits for better accuracy.

--- generate_answer.py
+++ generate_answer.py
@@ -29,6 +29,9 @@ import threading
 INPUT_PATH = Path("cse_476_final_project_test_data.json")
 OUTPUT_PATH = Path("cse_476_final_project_answers.json")
 
+# INPUT_PATH = Path("testing_questions.json")
+# OUTPUT_PATH = Path("testing_answers.json")
+
 API_KEY  = os.getenv("OPENAI_API_KEY", "cse476")
 API_BASE = os.getenv("API_BASE", "http://10.4.58.53:41701/v1")  
 MODEL    = os.getenv("MODEL_NAME", "bens_model")        
